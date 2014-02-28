1bf69653c67a90f28dcb0c33ff6c58253f3c31ec
Luis Rivas Vañó <luinux@gmail.com>
2014-02-28 13:07:58 -0500

    Renaming .tmux.conf to tmux.conf so it is visible when cloning

--- batinstall.sh
+++ batinstall.sh
@@ -2,7 +2,7 @@ read -p "this will overwrite your tmux config, proceed? Y/n" -r -n 1
 if [[ ! $REPLY =~ ^[nN]$ ]]
 then
   rm ~/.tmux.conf
-  ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+  ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
   echo -e "\033[0;33m"
   cat ./success.txt 
