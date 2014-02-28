f3955db94cabc7294a48f188224f2735be99fb91
daneryl <daneryl@gmail.com>
2014-02-27 21:45:24 -0500

    first tmux config with installer

---
+++ batinstall.sh
@@ -0,0 +1,11 @@
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+#if [ ! -w ~/.viminfo ] 
+#then 
+#echo "\033[31myou need write permission for .viminfo file\n\033[0m"
+#fi
+#make -C ~/.bat_vim/bundle/vimproc.vim/
+
+#echo "\033[0;33m"
+#cat ./success.txt 
+#echo "\033[0m"
