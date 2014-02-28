77486e7378fc27fb30fe1112bc7a49631ae62181
Luis Rivas Vañó <luinux@gmail.com>
2014-02-28 13:49:42 -0500

    Adding an specific .profile for tmux ready for sharing the session, and adding a tmux-session alias to global .profile

--- batinstall.sh
+++ batinstall.sh
@@ -1,8 +1,14 @@
-read -p "this will overwrite your tmux config, proceed? Y/n" -r -n 1
+read -p "this will overwrite your tmux config, and add an alias to your .profile, proceed? Y/n" -r -n 1
 if [[ ! $REPLY =~ ^[nN]$ ]]
 then
-  rm ~/.tmux.conf
+  rm -f ~/.tmux.conf
   ln -s $(pwd)/tmux.conf ~/.tmux.conf
+
+  rm -f ~/.profile-tmux
+  ln -s $(pwd)/profile-tmux ~/.profile-tmux
+
+  echo "Adding tmux-session alias to your .profile..."
+  grep -q 'alias tmux-session' ~/.profile && sed "s/^alias tmux-session.*$/alias tmux-session='tmux -S \/tmp\/pairing -2 attach -t Session'/" -i ~/.profile || echo "alias tmux-session='tmux -S /tmp/pairing -2 attach -t Session'" >> ~/.profile
 
   echo -e "\033[0;33m"
   cat ./success.txt 
